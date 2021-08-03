9bcbe9910cb0dbe2d8c95c1b4d6850f08cd8309e
ctrl-z-9000-times <dam1784@rit.edu>
2021-08-03 15:34:46 -0400

    new file:   neuwon/database/examples/life/unit_test.py

---
+++ neuwon/database/examples/life/unit_test.py
@@ -0,0 +1,24 @@
+from neuwon.database.examples.life.model import GameOfLife
+
+def test():
+    model = GameOfLife((10, 10))
+    model.randomize(.33)
+    for step in range(100):
+        model.advance()
+
+def test_by_num_alive():
+    size = 50
+    model = GameOfLife((size, size))
+    model.randomize(.33)
+    num_cells = size * size
+    sparsity = lambda: model.get_num_alive() / num_cells
+
+    assert (.30 < sparsity() < .36)
+
+    for _ in range(3): model.advance()
+
+    assert (.20 < sparsity() < .33)
+
+    for _ in range(500): model.advance()
+
+    assert (.01 < sparsity() < .05)
