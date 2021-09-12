9f60c2ef513a950fae44dc386415ee36209b23a9
ctrl-z-9000-times <dam1784@rit.edu>
2021-09-12 12:54:32 -0400

    modified:   neuwon/nmodl/tests/test_nmodl.py

--- neuwon/nmodl/tests/test_nmodl.py
+++ neuwon/nmodl/tests/test_nmodl.py
@@ -3,24 +3,26 @@ from neuwon.database import Database
 from neuwon.nmodl import NmodlMechanism
 import pytest
 
-def test_hh():
-    hh = NmodlMechanism("./nmodl_library/hh.mod", use_cache=False)
+def hh_smoke_test():
+    dt = .1
     db = Database()
+    hh = NmodlMechanism("./nmodl_library/hh.mod", use_cache=False)
     Segment = SegmentMethods._initialize(db)
     db_cls = Segment.get_database_class()
-    db_cls.add_attribute("conductances_na")
-    db_cls.add_attribute("conductances_k")
-    db_cls.add_attribute("conductances_l")
+    db_cls.add_attribute("na_conductance")
+    db_cls.add_attribute("k_conductance")
+    db_cls.add_attribute("l_conductance")
 
-    hh = hh.initialize(db, time_step=.1, celsius=6.3)
+    hh = hh.initialize(db, time_step=dt, celsius=6.3)
 
     my_seg = Segment(None, [0,0,0], 12)
     my_hh  = hh(my_seg, scale=.2)
     hh.advance()
     hh.advance()
     hh(Segment(None, [40,0,0], 12), scale=.2)
-    for _ in range(4):
+    for _ in range(40):
         hh.advance()
+    db.check()
 
 @pytest.mark.skip()
 def test_kinetic_model():
