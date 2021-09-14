f3a69b5ac6ff11acb5c102b03681ece288d9a471
ctrl-z-9000-times <dam1784@rit.edu>
2021-09-14 12:52:10 -0400

    modified:   neuwon/examples/mockup/__main__.py modified:   neuwon/examples/mockup/model.py

--- neuwon/examples/mockup/__main__.py
+++ neuwon/examples/mockup/__main__.py
@@ -4,17 +4,19 @@ from neuwon.regions import Cylinder
 from neuwon.segment import SegmentMethods
 from neuwon.examples.mockup.model import Excitatory
 
-diameter=100
-height=20
+diameter = 200
+height = 20
 region = Cylinder([0,0,0], [0,height,0], diameter/2)
 
 db = Database()
 SegmentMethods._initialize(db)
 Segment = db.get("Segment").get_instance_type()
 my_cell_type = Excitatory(Segment, region)
-my_cell_type.grow(3)
+my_cell_type.grow(10)
+
+# help(Segment)
 
 view = Viewport()
-view.set_scene(db)
+view.set_scene(db, 5)
 while True:
     view.tick()
--- neuwon/examples/mockup/model.py
+++ neuwon/examples/mockup/model.py
@@ -1,25 +1,44 @@
 from neuwon.growth import GrowthRoutine, Soma, Tree
 from neuwon.database import *
 
+default_parameters = {
+    "soma_diameter": 8,
+    "dendrite_morphology": {
+        "carrier_point_density": .0003,
+        "balancing_factor": .7,
+        "extension_distance": 40,
+        "bifurcation_distance": 40,
+        "maximum_segment_length": 20,
+        "diameter": 1.5,
+    },
+    "dendrite_mechanisms": {
+        "hh": {},
+    }
+}
+
 class NeuronType(GrowthRoutine):
+    def __init__(self, database, name, soma_region):
+        self.db_cls = database.add_class(name)
+        self.db_cls.add_attribute("root", dtype="Segment")
+
+    def get_all(self):
+        return self.db_cls.get_all_instances()
+
     def grow_synapses(self):
         # Must grow synapses in a separate pass, after all dendrites & axons
         # have been build for all cell types.
         1/0
 
 class Excitatory(NeuronType):
-    def __init__(self, Segment, region, **kwargs):
+    def __init__(self, Segment, region, parameters={}):
+        combined_params = dict(default_parameters)
+        combined_params.update(parameters)
         self.soma = Soma(Segment, region, 8)
-        self.dendrites = Tree(self.soma, region, .0003,
-                balancing_factor = .7,
-                extension_distance = 40,
-                bifurcation_distance = 40,
+        self.dendrites = Tree(self.soma, region,
+                **combined_params["dendrite_morphology"])
+        self.axon = Tree(self.soma, region, .001,
                 extend_before_bifurcate = False,
                 only_bifurcate = True,
-                maximum_segment_length = 20,
-                diameter = 1.5,
-        )
-        self.axon = Tree(self.soma, region, .0002,
                 diameter = .9,)
 
     def grow(self, num_cells=1):
