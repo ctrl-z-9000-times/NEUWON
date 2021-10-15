0171bb1003acabd7273e2871c1dd08e55ba7a98c
ctrl-z-9000-times <dam1784@rit.edu>
2021-10-15 17:42:30 -0400

    functions calling functions

--- neuwon/database/methods.py
+++ neuwon/database/methods.py
@@ -3,6 +3,7 @@ from neuwon.database.database import DB_Class, DB_Object
 from neuwon.database.data_components import ClassAttribute, Attribute, SparseMatrix
 from neuwon.database.doc import Documentation
 from neuwon.database.dtypes import *
+from neuwon.database.memory_spaces import host, cuda
 import ast
 import copy
 import inspect
@@ -19,26 +20,18 @@ class Function(Documentation):
         if isinstance(function, Function): function = function.original
         Documentation.__init__(self, function.__name__, inspect.getdoc(function))
         self.original = function
+        self.host_jit = None
+        self.cuda_jit = None
 
     def __call__(self, *args, **kwargs):
-        return self.original(*args, **kwargs)
-
-    def _disassemble_function(self):
-        self.filename  = inspect.getsourcefile(self.original)
-        self.signature = inspect.signature(self.original)
-        self.body_text = io.StringIO()
-        uncompyle6.code_deparse(self.original.__code__, out=self.body_text)
-        self.body_text = self.body_text.getvalue()
-        self.body_ast  = ast.parse(self.body_text, self.filename)
-        self.globals   = self.original.__globals__
+        if self.host_jit is None:
+            self.host_jit = _DisassembleFunction(self.original).jit('host')
+        return self.host_jit(*args, **kwargs)
 
 class Method(Function):
     def __init__(self, function):
         Function.__init__(self, function)
-        self._disassemble_function()
-        self.self_variable = next(iter(self.signature.parameters))
         self.db_class = None
-        self.first_call = True
 
     def _register_method(self, db_class, add_attr=True):
         assert isinstance(db_class, DB_Class)
@@ -50,6 +43,8 @@ class Method(Function):
             assert self.name not in self.db_class.methods
             self.db_class.methods[self.name] = self
             setattr(self.db_class.instance_type, self.name, lambda inst=None: self.__call__(inst))
+            # TODO: This should use a property so that it can show documentation!
+            #       And maybe then I wont need the lambda?
 
     def __call__(self, instances=None, *args, **kwargs):
         """
@@ -60,7 +55,16 @@ class Method(Function):
                 * None, in which case this method is called on all instances.
         """
         assert self.db_class is not None
-        if self.first_call: self._jit()
+        target = self.db_class.database.memory_space
+        if target is host and self.host_jit is None:
+            1/0 # TODO: JIT!
+            dis = _MethodFunction(self.original, self.db_class)
+            dis.to_soa()
+            # dis.
+            self.db_arguments   = dis.arguments
+
+        elif target is cuda and self.cuda_jit is None:
+            1/0 # TODO: JIT!
         db_args = [self.db_class.get_data(x) for x in self.db_arguments]
         if instances is None:
             instances = range(0, len(self.db_class))
@@ -73,18 +77,53 @@ class Method(Function):
             assert isinstance(instances, self.db_class.instance_type)
             return self.function(instances._idx, *db_args, *args, **kwargs)
 
-    def _jit(self):
-        transformer         = _OOP_to_SoA(self)
-        self.function       = numba.njit(transformer.function)
-        self.db_arguments   = transformer.arguments
-        self.first_call     = False
+class _DisassembleFunction:
+    def __init__(self, function):
+        assert isinstance(function, Callable)
+        assert not isinstance(function, Function)
+        self.original  = function
+        self.name      = self.original.__name__.replace('<', '_').replace('>', '_')
+        self.filename  = inspect.getsourcefile(self.original)
+        self.signature = inspect.signature(self.original)
+        self.body_text = io.StringIO()
+        uncompyle6.code_deparse(self.original.__code__, out=self.body_text)
+        self.body_text = self.body_text.getvalue()
+        self.body_ast  = ast.parse(self.body_text, self.filename)
+        nonlocals, globals, builtins, unbound = inspect.getclosurevars(self.original)
+        self.closure = {}
+        self.closure.update(builtins)
+        self.closure.update(globals)
+        self.closure.update(nonlocals)
+
+    def jit(self, target='host'):
+        for k, v in self.closure.items():
+            if isinstance(v, Function):
+                self.closure[k] = _DisassembleFunction(v.original).jit(target)
+
+        self.assemble_function()
+
+        if target == 'host':
+            return numba.njit(self.function)
+        elif target == 'cuda':
+            return numba.cuda.njit(self.function)
+        else: raise NotImplementedError(target)
+
+    def assemble_function(self):
+        template                = f"def {self.name}{self.signature}:\n pass\n"
+        module_ast              = ast.parse(template, filename=self.filename)
+        self.function_ast       = module_ast.body[0]
+        self.function_ast.body  = self.body_ast.body
+        self.module_ast = ast.fix_missing_locations(module_ast)
+        exec(compile(self.module_ast, self.filename, mode='exec'), self.closure)
+        self.function = self.closure[self.name]
 
 
-class _OOP_to_SoA(ast.NodeTransformer):
+class _DisassembleMethod(_DisassembleFunction, ast.NodeTransformer):
     def __init__(self, method):
+        _DisassembleFunction.__init__(self, method, db_class)
         ast.NodeTransformer.__init__(self)
-        assert isinstance(method, Method)
         self.method         = method
+        self.self_var  = next(iter(self.signature.parameters))
         self.method_calls   = set()
         self.loads          = set()
         self.stores         = set()
@@ -122,7 +161,7 @@ class _OOP_to_SoA(ast.NodeTransformer):
         else: raise NotImplementedError(type(value))
         access = tuple(reversed(access))
         # 
-        if access[0] != self.method.self_variable:
+        if access[0] != self.self_var:
             return node
         if isinstance(ctx, ast.Del):
             raise TypeError("Can not 'del' database attributes.")
@@ -160,7 +199,7 @@ class _OOP_to_SoA(ast.NodeTransformer):
             elif isinstance(attr, Attribute):
                 load_stmts.append(f"{self.local_name(attr)} = {self.global_name(attr)}[_idx]")
             else: raise NotImplementedError(type(attr))
-        load_ast = ast.parse("\n".join(load_stmts), filename=self.method.filename)
+        load_ast = ast.parse("\n".join(load_stmts), filename=self.filename)
         self.body_ast.body = load_ast.body + self.body_ast.body
 
     def append_stores(self):
@@ -170,7 +209,7 @@ class _OOP_to_SoA(ast.NodeTransformer):
                 store_stmts.append(f"{self.global_name(attr)}[_idx] = {self.local_name(attr)}")
             else:
                 raise TypeError(f"Can not assign to '{attr.qualname}' in this context.")
-        store_ast = ast.parse("\n".join(store_stmts), filename=self.method.filename)
+        store_ast = ast.parse("\n".join(store_stmts), filename=self.filename)
         self.body_ast.body.extend(store_ast.body)
 
     def get_arguments(self):
@@ -179,11 +218,11 @@ class _OOP_to_SoA(ast.NodeTransformer):
         return arguments
 
     def assemble_function(self):
-        signature = re.subn(rf'\b{self.method.self_variable}\b',
+        signature = re.subn(rf'\b{self.self_var}\b',
                             ', '.join(self.get_arguments()),
                             str(self.method.signature), count = 1)[0]
         template                = f"def {self.method.name}{signature}:\n pass\n"
-        module_ast              = ast.parse(template, filename=self.method.filename)
+        module_ast              = ast.parse(template, filename=self.filename)
         self.function_ast       = module_ast.body[0]
         self.function_ast.body  = self.body_ast.body
         self.module_scope       = dict(self.method.globals)
@@ -193,18 +232,26 @@ class _OOP_to_SoA(ast.NodeTransformer):
         import astor
         print(type(self), "Compiling AST ...")
         print(astor.to_source(self.module_ast))
-        exec(compile(self.module_ast, self.method.filename, mode='exec'), self.module_scope)
+        exec(compile(self.module_ast, self.filename, mode='exec'), self.module_scope)
         self.function = self.module_scope[self.method.name]
 
 
-class _MethodInMethod(_OOP_to_SoA):
-
+class _MethodInMethod(_DisassembleMethod):
+    """
+    This class is used to inline methods by nesting them like:
+    >>> def foo(_idx, ...):
+    >>>     self_x = ...
+    >>>     def bar():
+    >>>         nonlocal self_x
+    >>>         self_x += 1
+    >>>     bar()
+    """
     def prepend_loads(self):
         # Declare variables as non-local's instead of reading from an array.
         load_stmts = []
         for attr in self.loads:
             load_stmts.append(f"nonlocal {self.local_name(attr)}")
-        load_ast = ast.parse("\n".join(load_stmts), filename=self.method.filename)
+        load_ast = ast.parse("\n".join(load_stmts), filename=self.filename)
         self.body_ast.body = load_ast.body + self.body_ast.body
 
     def append_stores(self):
--- neuwon/database/tests/test_methods.py
+++ neuwon/database/tests/test_methods.py
@@ -1,7 +1,6 @@
 from neuwon.database import Database, Function, Method
 import math
 import pytest
-import random
 
 
 def test_basic_function():
@@ -17,6 +16,18 @@ def test_basic_function():
     assert qq() == 42
 
 
+def test_functions_calling_functions():
+    @Function
+    def foo(x):
+        return bar(x) + 3
+    @Function
+    def bar(x):
+        return x - 3
+    assert foo(4) == 4
+    assert bar(4) == 1
+
+
+# @pytest.mark.skip
 def test_basic_method():
     class Seg:
         __slots__ = ()
@@ -35,10 +46,11 @@ def test_basic_method():
     assert my_seg.v == -78
 
 
+# @pytest.mark.skip
 def test_calling_methods():
     class Seg:
         __slots__ = ()
-        @Function
+        @Method
         def foo(self):
             self.v += 4
             self.bar()
@@ -60,27 +72,32 @@ def test_calling_methods():
     assert my_seg.v == -74
 
 
-@Function
-def area_eq(r):
-    return math.pi * (r**2)
-
-class Segment:
-    __slots__ = ()
-    @classmethod
-    def initialize(cls, db):
-        seg_data = db.add_class("Segment", cls)
-        seg_data.add_attribute("r", 33.3)
-        seg_data.add_attribute("area")
-        return seg_data.get_instance_type()
-
+x = 5
+# @pytest.mark.skip
+def test_calling_functions():
+    # y = 0
     @Function
-    def _compute_area(self):
-        self.area = area_eq(self.r)
+    def area_eq(r):
+        return math.pi * (r**2)
+    class Segment:
+        __slots__ = ()
+        @classmethod
+        def initialize(cls, db):
+            seg_data = db.add_class("Segment", cls)
+            seg_data.add_attribute("d", 33.3)
+            seg_data.add_attribute("area")
+            return seg_data.get_instance_type()
+        @Method
+        def _compute_area(self):
+            self.area = area_eq(self.d / 2 + x + y)
 
-@pytest.mark.skip
-def test_compile():
     db = Database()
     Seg = Segment.initialize(db)
-    for _ in range(99): Seg()
+    y = -5 # Test late initialize/JIT.
+    for _ in range(9): Seg()
+    my_seg = Seg()
+    my_seg.d = 12
+    for _ in range(9): Seg()
     Seg._compute_area()
+    assert my_seg.area == pytest.approx(math.pi * 36)
 
