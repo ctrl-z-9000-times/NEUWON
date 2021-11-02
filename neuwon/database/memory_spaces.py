4d14ad9ad9d8ac9ef6615813a283ce7fd07d1600
ctrl-z-9000-times <dam1784@rit.edu>
2021-11-01 22:49:10 -0400

    make MemorySpace an Enum

--- neuwon/database/memory_spaces.py
+++ neuwon/database/memory_spaces.py
@@ -1,34 +1,30 @@
-"""
-All memory spaces are global singletons.
-"""
-
 import cupy
 import cupyx.scipy.sparse
-import numpy
-import scipy.sparse
+import enum
 import numba
 import numba.cuda
+import numpy
+import scipy.sparse
 
-available = dict()
+class MemorySpace(enum.Enum):
+    host = (numpy, scipy.sparse, numba.njit)
+    cuda = (cupy, cupyx.scipy.sparse, numba.cuda.jit)
 
-class MemorySpace:
-    def __init__(self, name, array_module, matrix_module, jit_wrapper):
-        self.name = str(name)
-        available[self.name] = self
+    def __init__(self, array_module, matrix_module, jit_wrapper):
         self.matrix_module   = matrix_module
         self.array_module    = array_module
         self.array           = array_module.array
         self.jit_wrapper     = jit_wrapper
 
     def __repr__(self):
-        return f"<MemorySpace: {self.name}>"
+        return f"<MemorySpace.{self.name}>"
 
     def get_name(self) -> str:  return self.name
     def get_array_module(self): return self.array_module
     def get_matrix_module(self):return self.matrix_module
 
-host = MemorySpace("host", numpy, scipy.sparse, numba.njit)
-cuda = MemorySpace("cuda", cupy, cupyx.scipy.sparse, numba.cuda.jit)
+for x in MemorySpace:
+    globals()[x.name] = x
 
 class ContextManager:
     def __init__(self, database, memory_space):
@@ -36,7 +32,7 @@ class ContextManager:
         if isinstance(memory_space, MemorySpace):
             self.memory_space = memory_space
         else:
-            self.memory_space = available[str(memory_space)]
+            self.memory_space = MemorySpace[str(memory_space)]
 
     def __enter__(self):
         self.prior_memory_space    = self.database.memory_space
