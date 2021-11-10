25bffbb1d6b2c2d69e5dd1e4cbfe0e7ce96d97af
ctrl-z-9000-times <dam1784@rit.edu>
2021-11-09 20:15:59 -0500

    Species Parameters - WIP

--- neuwon/species/tests/test_species.py
+++ neuwon/species/tests/test_species.py
@@ -3,6 +3,31 @@ from neuwon.database.time import Clock
 from neuwon.segment import SegmentMethods
 from neuwon.species import *
 
+test_parameters = {
+    'only_e': {
+        'reversal_potential': -60,
+    },
+    'global_const_species': {
+        'inside': {
+            'concentration': 11,
+            'global_constant': True,
+        },
+    },
+    'non_diffusive_species': {
+        'inside': {
+            'concentration': 22.2,
+            'diffusivity': 0.0,
+        },
+    },
+    'full_species': {
+        'inside': {
+            'concentration': 33,
+            'diffusivity': 0.01,
+            'decay_period': 77.7,
+        },
+    },
+}
+
 def test_init():
     db = Database()
     SegmentMethods._initialize(db,
@@ -11,7 +36,6 @@ def test_init():
             membrane_capacitance = 1,)
 
     s = Species("leak", reversal_potential = -60)
-    inclock = Clock(.5)
     s._initialize(db, .5, 8, inclock)
     inclock.tick()
     s._advance(db)
