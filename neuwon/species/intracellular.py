e95fad054f78c75a4473ca3941b6d7b7386fe57a
ctrl-z-9000-times <dam1784@rit.edu>
2021-11-12 09:17:16 -0500

    new file:   neuwon/species/intracellular.py

---
+++ neuwon/species/intracellular.py
@@ -0,0 +1,50 @@
+
+
+class InsideMethods:
+    @classmethod
+    def _initialize(cls):
+        db.add_archetype("inside", doc="Intracellular space.")
+        db.add_attribute("membrane/inside", dtype="inside", doc="""
+                A reference to the outermost shell.
+                The shells and the innermost core are allocated in a contiguous block
+                with this referencing the start of range of length "membrane/shells" + 1.
+                """)
+        db.add_attribute("membrane/shells", dtype=np.uint8)
+        db.add_attribute("inside/membrane", dtype="membrane")
+        db.add_attribute("inside/shell_radius", units="μm")
+        db.add_attribute("inside/volumes",
+                # bounds=(epsilon * (1e-6)**3, None),
+                allow_invalid=True,
+                units="Liters")
+        db.add_sparse_matrix("inside/neighbor_distances", "inside")
+        db.add_sparse_matrix("inside/neighbor_border_areas", "inside")
+
+    def _inside_diffusion_coefficients(self, access):
+        dt      = access("time_step") / 1000 / _ITERATIONS_PER_TIMESTEP
+        parents = access("membrane/parents").get()
+        lengths = access("membrane/lengths").get()
+        xareas  = access("membrane/cross_sectional_areas").get()
+        volumes = access("membrane/inside/volumes").get()
+        if self.use_shells: raise NotImplementedError
+        src = []; dst = []; coef = []
+        for location in range(len(parents)):
+            parent = parents[location]
+            if parent == NULL: continue
+            flux = self.inside_diffusivity * xareas[location] / lengths[location]
+            src.append(location)
+            dst.append(parent)
+            coef.append(+dt * flux / volumes[parent])
+            src.append(location)
+            dst.append(location)
+            coef.append(-dt * flux / volumes[location])
+            src.append(parent)
+            dst.append(location)
+            coef.append(+dt * flux / volumes[location])
+            src.append(parent)
+            dst.append(parent)
+            coef.append(-dt * flux / volumes[parent])
+        for location in range(len(parents)):
+            src.append(location)
+            dst.append(location)
+            coef.append(-dt / self.inside_decay_period)
+        return (coef, (dst, src))
