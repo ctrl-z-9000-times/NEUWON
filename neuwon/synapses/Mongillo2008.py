b031caa4b0ccf190f4c37c81fecf6ed50dc0dccd
ctrl-z-9000-times <dam1784@rit.edu>
2021-11-28 15:20:30 -0500

    new file: neuwon/synapses/Mongillo2008.py - untested

---
+++ neuwon/synapses/Mongillo2008.py
@@ -0,0 +1,61 @@
+import math
+from neuwon.database import Compute, Real, epsilon
+
+class Mongillo2008:
+    @staticmethod
+    def initialize(synapse_data, **,
+            minimum_utilization,
+            utilization_decay,
+            resource_recovery,):
+        synapse_data.add_class_attribute('minimum_utilization',
+                initial_value = minimum_utilization,
+                valid_range = (0.0, 1.0),
+                doc = "")
+        synapse_data.add_class_attribute('utilization_decay',
+                initial_value = utilization_decay,
+                valid_range = (epsilon, math.inf),
+                doc = "")
+        synapse_data.add_class_attribute('resource_recovery',
+                initial_value = resource_recovery,
+                valid_range = (epsilon, math.inf),
+                doc = "")
+        synapse_data.add_attribute('last_update',
+                initial_value = 0.0,
+                valid_range = (0.0, math.inf),
+                doc = "")
+        synapse_data.add_attribute('resources',
+                initial_value = 1.0,
+                valid_range = (0.0, 1.0),
+                doc = "")
+        synapse_data.add_attribute('utilization',
+                initial_value = minimum_utilization,
+                valid_range = (0.0, 1.0),
+                doc = "")
+
+    @Compute
+    def reset(self):
+        self.last_update = 0.0
+        self.resources   = 1.0
+        self.utilization = self.minimum_utilization
+
+    @Compute
+    def compute(self, timestamp) -> Real:
+        # Unpack the synapse parameters & data.
+        utilization         = self.utilization
+        resources           = self.resources
+        minimum_utilization = self.minimum_utilization
+        # Compute temporal effects, since last activation of this synapse.
+        elapsed_time = timestamp - self.last_update
+        utilization_alpha = math.exp( -elapsed_time / self.utilization_decay)
+        resources_alpha   = math.exp( -elapsed_time / self.resource_recovery)
+        utilization += utilization_alpha * (minimum_utilization - utilization)
+        resources   += resources_alpha * (1.0 - resources)
+        # Compute fractional neurotransmitter release.
+        syn_release   = utilization * resources
+        utilization  += minimum_utilization * (1.0 - utilization)
+        resources    -= syn_release
+        # Update synapse data and return.
+        self.last_update = timestamp
+        self.utilization = utilization
+        self.resources   = resources
+        return syn_release
