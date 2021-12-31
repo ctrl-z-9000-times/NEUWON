7b1948f3240db4e2a3a5bef02e923e2638565f4b
ctrl-z-9000-times <dam1784@rit.edu>
2021-12-31 12:16:49 -0500

    modified:   neuwon/gui/tests/test_rotate_align.py

--- neuwon/gui/rotate_align.py
+++ neuwon/gui/rotate_align.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+epsilon = np.finfo(np.float32).eps
+
 def magnitude(vector):
     return math.sqrt(vector.dot(vector))
 
@@ -8,12 +10,13 @@ def rotate_align(a, b):
     # https://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d/476311#476311
     a = a / magnitude(a)
     b = b / magnitude(b)
-    v = np.cross(a, b)
     c = np.dot(a, b) # Cosine of angle
-    if c == 0:
+    c1 = (1.0 + c)
+    if abs(c1) < epsilon:
         raise NotImplementedError
+    v = np.cross(a, b)
     vx = np.array([ # Skew symmetric cross-product matrix
         [0,    -v[2], v[1]],
         [v[2],  0,   -v[0]],
         [-v[1], v[0], 0]])
-    return np.eye(3) + vx + np.matmul(vx, vx) / (1.0 + c)
+    return np.eye(3) + vx + np.matmul(vx, vx) / c1
--- neuwon/gui/tests/test_rotate_align.py
+++ neuwon/gui/tests/test_rotate_align.py
@@ -2,16 +2,31 @@ from neuwon.gui.rotate_align import rotate_align, magnitude
 import numpy as np
 import pytest
 
+def align_and_check(v1, v2):
+    """ Rotate v1 to align to v2, and then check that they're aligned correctly. """
+    rot_matrix = rotate_align(v2, v1)
+    v3 = v1.dot(rot_matrix)
+    v3 = np.squeeze(np.asarray(v3))
+    v2_norm = v2 / magnitude(v2)
+    assert magnitude(v1) == pytest.approx(magnitude(v3))
+    v3_norm = v3 / magnitude(v3)
+    assert v3_norm[0] == pytest.approx(v2_norm[0])
+    assert v3_norm[1] == pytest.approx(v2_norm[1])
+    assert v3_norm[2] == pytest.approx(v2_norm[2])
+
 def test_random_vectors():
     for trial in range(1000):
         v1 = np.random.uniform(-100, 100, size=3) # Reference vector
         v2 = np.random.uniform(-100, 100, size=3) # Target vector
-        rot_matrix = rotate_align(v2, v1)
-        v3 = v1.dot(rot_matrix)
-        v3 = np.squeeze(np.asarray(v3))
-        v2_norm = v2 / magnitude(v2)
-        assert magnitude(v1) == pytest.approx(magnitude(v3))
-        v3_norm = v3 / magnitude(v3)
-        assert v3_norm[0] == pytest.approx(v2_norm[0])
-        assert v3_norm[1] == pytest.approx(v2_norm[1])
-        assert v3_norm[2] == pytest.approx(v2_norm[2])
+        align_and_check(v1, v2)
+
+def test_identical_vectors():
+    for trial in range(3):
+        v1 = np.random.uniform(-100, 100, size=3) # Reference vector
+        align_and_check(v1, v1 * np.random.uniform(1e-3, 1e3))
+
+@pytest.mark.skip()
+def test_opposite_vectors():
+    for trial in range(3):
+        v1 = np.random.uniform(-100, 100, size=3) # Reference vector
+        align_and_check(v1, -v1)
