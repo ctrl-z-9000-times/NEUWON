1687e640a3a438fc463345d09d8e1d15cf78df24
ctrl-z-9000-times <dam1784@rit.edu>
2022-02-06 15:26:15 -0500

    nmodl testcase for AMPA5 - WIP

--- neuwon/rxd/nmodl/tests/test_nmodl.py
+++ neuwon/rxd/nmodl/tests/test_nmodl.py
@@ -33,6 +33,38 @@ def test_hh_smoke_test():
     m.database.check()
     assert my_seg.na_conductance > 0
 
+
+@pytest.mark.skip()
+def test_ampa5_smoke_test():
+    m = RxD_Model(time_step = .1,
+        mechanisms={
+            'ampa': NMODL("./nmodl_library/Destexhe1994/ampa5.mod", use_cache=False)},
+        species={
+            'glu': {'outside': {'diffusivity': 0.005},},
+        },
+    )
+    m.database.check()
+
+    ampa = m.mechanisms['ampa']
+    help(ampa)
+    print('PARAMETERS:', ampa._parameters)
+    print('ADVANCE PYCODE:')
+    print(ampa._advance_pycode)
+
+    ampa.advance()
+
+    my_seg = m.Neuron([0,0,0], 12).root
+    my_ampa = ampa(my_seg, .2)
+    # assert my_seg.l_conductance == 0
+    # ampa.advance()
+    # assert my_seg.l_conductance > 0
+    # ampa(m.Neuron([40,0,0], 12).root, scale=.2)
+    # for _ in range(40):
+    #     ampa.advance()
+    # m.database.check()
+    # assert my_seg.na_conductance > 0
+
+
 @pytest.mark.skip()
 def test_kinetic_model():
     nav11 = NMODL("./nmodl_library/Balbi2017/Nav11_a.mod", use_cache=False)
