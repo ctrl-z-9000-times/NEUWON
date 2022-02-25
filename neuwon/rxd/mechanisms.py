d07adf0be93d3d3820b5c3fe964bef35bd1c717d
ctrl-z-9000-times <dam1784@rit.edu>
2022-02-25 08:07:26 -0500

    add_mechanisms accept parameters dict

--- neuwon/rxd/mechanisms.py
+++ neuwon/rxd/mechanisms.py
@@ -51,12 +51,14 @@ class LocalMechanism:
     @classmethod
     def advance(cls):
         """ Advance all instances of this mechanism. """
-        raise NotImplementedError(type(self))
+        raise NotImplementedError(cls)
+
+Mechanism = LocalMechanism # TODO: remove this alias.
 
 class MechanismsFactory(dict):
-    def __init__(self, rxd_model, parameters:dict):
+    def __init__(self, model, parameters:dict):
         super().__init__()
-        self._rxd_model = rxd_model
+        self._model = model
         self.add_parameters(parameters)
 
     def add_parameters(self, parameters:dict) -> [Mechanism]:
@@ -64,15 +66,24 @@ class MechanismsFactory(dict):
                     for name, mechanism in parameters.items()]
 
     def add_mechanism(self, name, mechanism) -> Mechanism:
+        """ """
+        # Unpack the specification & parameters.
         name = str(name)
         assert name not in self
         if isinstance(mechanism, str):
-            if mechanism.endswith(".mod"):
-                mechanism = neuwon.rxd.nmodl.NMODL(mechanism)
-            else:
-                raise ValueError("File extension not understood")
+            parameters = {}
+        else:
+            mechanism, parameters = mechanism
+        # Create the Mechanism object.
+        if mechanism.endswith(".mod"):
+            mechanism = neuwon.rxd.nmodl.NMODL(mechanism, parameters)
+        elif mechanism.endswith(".py"):
+            1/0 # TODO?
+        else:
+            raise ValueError("File extension not understood")
+        # 
         if hasattr(mechanism, "initialize"):
-            retval = mechanism.initialize(self._rxd_model, name)
+            retval = mechanism.initialize(self._model, name)
             if retval is not None:
                 mechanism = retval
         self[name] = mechanism
