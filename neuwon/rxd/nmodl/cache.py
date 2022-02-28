5b6be8637c0e43c83790baf4d6630a3a1e0c6f76
ctrl-z-9000-times <dam1784@rit.edu>
2022-02-27 22:19:25 -0500

    modified:   neuwon/rxd/nmodl/cache.py

--- neuwon/rxd/nmodl/cache.py
+++ neuwon/rxd/nmodl/cache.py
@@ -18,21 +18,22 @@ def try_loading(filename, obj):
         nmodl_ts  = getmtime(filename)
         cache_ts  = getmtime(cache_file)
     except FileNotFoundError: return False
-    python_ts = getmtime(__file__)
     if nmodl_ts > cache_ts: return False
-    if python_ts > cache_ts: return False
     try:
-        with open(cache_file, 'rb') as f: data = pickle.load(f)
+        with open(cache_file, 'rb') as f:
+            cache_obj = pickle.load(f)
     except Exception as err:
         print("Warning: nmodl cache read failed:", str(err), file=stderr)
         return False
-    obj.__dict__.update(data)
+    obj.__class__ = cache_obj.__class__
+    obj.__dict__.update(cache_obj.__dict__)
     return True
 
 def save(filename, obj):
     cache_dir, cache_file = _dir_and_file(filename)
     try:
         os.makedirs(cache_dir, exist_ok=True)
-        with open(cache_file, 'wb') as f: pickle.dump(obj.__dict__, f)
+        with open(cache_file, 'wb') as f:
+            pickle.dump(obj, f)
     except Exception as x:
         print("Warning: nmodl cache write failed:", str(x), file=stderr)
