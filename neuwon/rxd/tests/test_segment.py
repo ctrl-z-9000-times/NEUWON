beaae5fb17fa4923b86511332f752974b2bc4e0b
ctrl-z-9000-times <dam1784@rit.edu>
2022-03-01 10:32:12 -0500

    merge neuwon/rxd/tests/test_insert.py into neuwon/rxd/tests/test_segment.py

--- neuwon/rxd/tests/test_segment.py
+++ neuwon/rxd/tests/test_segment.py
@@ -1,4 +1,5 @@
 from neuwon.database import Database
+from neuwon.rxd import RxD_Model
 from neuwon.rxd.neuron.neuron import Neuron as NeuronSuperclass
 import pytest
 
@@ -46,3 +47,23 @@ def test_swc():
     assert pct_diff(surface, 6621.44) <= .05
     assert pct_diff(volume,  3500.58) <= .05
     db.check()
+
+def test_basic_insertion():
+    my_model = RxD_Model(
+        mechanisms= {
+            'hh': './nmodl_library/hh.mod',
+        },)
+    my_neuron   = my_model.Neuron([0,0,0], 10)
+    my_root     = my_neuron.root
+    my_segments = my_root.add_section([100,0,0], 3, maximum_segment_length=10)
+
+    my_root.insert({})
+    instances = my_root.insert({'hh': 1})
+
+    assert instances['hh'].get_database_class().get_name() == 'hh'
+    my_model.check()
+
+@pytest.mark.skip
+def test_other_mechanisms():
+    # TODO: Test inserting mechanisms that reference each other.
+    1/0
