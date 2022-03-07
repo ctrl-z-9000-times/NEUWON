5bebb514a80c7c51552c6e321b016e0b52341145
ctrl-z-9000-times <dam1784@rit.edu>
2022-03-07 10:12:06 -0500

    Rework extracellular to not use voronoi cells.

--- neuwon/rxd/extracellular.py
+++ neuwon/rxd/extracellular.py
@@ -1,10 +1,7 @@
 from neuwon.database import Real, epsilon, Pointer, NULL
-from neuwon.rxd.voronoi import voronoi_cell
-from scipy.spatial import KDTree
 import numpy as np
 
 class Extracellular:
-    """ Extracellular Space with a voronoi diagram. """
     __slots__ = ()
     @staticmethod
     def _initialize(database,
@@ -12,8 +9,8 @@ class Extracellular:
                 maximum_distance = 20,):
         ecs_data = database.add_class(Extracellular)
         ecs_data.add_attribute('coordinates', shape=(3,), units='μm')
-        ecs_data.add_attribute('voronoi_volume', units='μm³')
         ecs_data.add_attribute('volume', units='μm³')
+        ecs_data.add_attribute('voxel', False, dtype=bool)
         ecs_data.add_class_attribute('tortuosity', tortuosity)
 
         ecs_data.add_sparse_matrix('neighbor_distances', Extracellular)
@@ -22,8 +19,7 @@ class Extracellular:
 
         ecs_cls = ecs_data.get_instance_type()
         ecs_cls.maximum_distance = float(maximum_distance)
-        ecs_cls._dirty = []
-        ecs_cls._kd_tree = None
+        ecs_cls._dirty = True
 
         segment_data = database.get_class('Segment')
         segment_data.add_attribute('outside', dtype=Extracellular, allow_invalid=True)
@@ -34,59 +30,20 @@ class Extracellular:
         self.coordinates = coordinates
         self.volume      = volume
         cls = type(self)
-        cls._dirty.append(self)
-        cls._kd_tree = None
+        cls._dirty = True
 
-    @property
-    def neighbors(self):
-        return self.neighbor_distances[0]
+    def get_voxel(self, coordinates):
+        # TODO: Return the voxel that contains the given coordinates.
+        #       If it doesn't exist yet then create it.
+        1/0
 
-    @classmethod
-    def _clean(cls):
-        if not cls._dirty: return
-        db_class    = cls.get_database_class()
-        assert db_class.get_database().is_sorted()
-        dirty = [ecs.get_unstable_index() for ecs in cls._dirty]
-        coordinates = db_class.get_data("coordinates")
-        cls._kd_tree = KDTree(coordinates)
-        cls._compute_voronoi_cells(dirty)
-        neighbors_matrix = db_class.get("neighbor_distances").to_lil().get_data()
-        touched = set()
-        for location in dirty:
-            neighbors = neighbors_matrix.getrow(location)
-            touched.update(neighbors.rows[0])
-        touched.difference_update(dirty)
-        cls._compute_voronoi_cells(list(touched))
-        cls._dirty = []
-
-    @classmethod
-    def _compute_voronoi_cells(cls, locations):
-        # TODO: Consider https://en.wikipedia.org/wiki/Power_diagram
-        db_class        = cls.get_database_class()
-        coordinates     = db_class.get_data("coordinates")
-        voronoi_volume  = db_class.get_data("voronoi_volume")
-        for location in locations:
-            potential_neighbors = cls._kd_tree.query_ball_point(
-                                    coordinates[location], 2 * cls.maximum_distance)
-            potential_neighbors.remove(location)
-            volume, neighbors = voronoi_cell(location,
-                                            cls.maximum_distance,
-                                            np.array(potential_neighbors, dtype=Pointer),
-                                            coordinates)
-            voronoi_volume[location] = volume
-            neighbor_locations = list(neighbors['location'])
-            neighbor_distances = list(neighbors['distance'])
-            neighbor_areas     = list(neighbors['border_surface_area'])
-            db_class.get("neighbor_distances").write_row(
-                    location, neighbor_locations, neighbor_distances)
-            db_class.get("neighbor_border_areas").write_row(
-                    location, neighbor_locations, neighbor_areas)
+        # divide the coordinates by the voxel dimensions, round down to integer,
+        # hash the vector of three integers. Keep a dict of hashes->voxels.
 
     @classmethod
     def _outside_diffusion_coefficients(cls, access):
-        1/0
-        extracellular_tortuosity = 1.4 # TODO: FIXME: put this one back in the db?
-        D = self.outside_diffusivity / extracellular_tortuosity ** 2
+        1/0 # TODO
+        D = self.outside_diffusivity / cls.tortuosity ** 2
         dt          = access("time_step") / 1000 / _ITERATIONS_PER_TIMESTEP
         decay       = -dt / self.outside_decay_period
         recip_vol   = (1.0 / access("outside/volumes")).get()
@@ -110,7 +67,3 @@ class Extracellular:
                 coef[write_idx] = -dt * flux * recip_vol[location] + decay
                 write_idx += 1
         return (coef, (dst, src))
-
-    @classmethod
-    def fill_region(cls, maximum_distance, region) -> list:
-        raise NotImplementedError
