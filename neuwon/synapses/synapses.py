5c281107b0a613a914c3c18db0c92c660313bcff
ctrl-z-9000-times <dam1784@rit.edu>
2022-03-13 15:59:31 -0400

    rewrote: neuwon/synapses/synapses.py

--- neuwon/synapses/synapses.py
+++ neuwon/synapses/synapses.py
@@ -1,61 +1,80 @@
-from neuwon.database import Pointer, NULL, Compute
-from neuwon.rxd import Mechanism
-from .constraints import Constraints
+# from .constraints import Constraints
 import math
+import numpy as np
+import random
+import scipy.spatial
 
-
-class Synapse(Mechanism):
-    def __init__(self, presynapse: 'Neuron', postsynapse: 'Segment'):
-        postsynapse._num_presyn += 1
-
-    @Compute
-    def postsynapse_event(self) -> bool:
-        over = self.postsynapse.voltage >= self.postsyn_threshold
-        event = over and not self.postsynapse_event_state
-        self.postsynapse_event_state = event
-        return event
-
-
-class SynapseGrowthProgram:
-    def __init__(self, factory, synapse_type, *,
-                transmitter,
-                constraints,
-                number = None,
-                number_per_neuron = None,
-                stp = None,
-                stdp = None,
+class Synapse:
+    """ """
+    __slots__ = ()
+    @classmethod
+    def _initialize(cls, model, synapse_type, *,
+                number=0,
+                maximum_distance=math.inf,
+                cleft=None,
+                attachment_points,
                 ):
-        self.name           = self.synapse_type = str(synapse_type)
-        self.factory        = factory
-        self.transmitter    = self.factory.rxd.species[str(transmitter)]
-        self.database       = self.factory.rxd.get_database()
-        self.constraints    = Constraints(self.database, **constraints)
-        self.Neuron   = self.database.get_class('Neuron')
-        self.Segment  = self.database.get_class('Segment')
-        self.Segment.get_database_class().add_attribute('_num_presyn', dtype=np.uint8)
+        database = model.get_database()
+        syn_data = database.add_class(str(synapse_type), cls)
+        syn_cls  = syn_data.get_instance_type()
+        syn_cls._maximum_distance = float(maximum_distance)
+        syn_cls._attachment_points = []
+        for index, parameters in enumerate(attachment_points):
+            syn_cls._initialize_attachment_point(syn_data, index, **parameters)
+        if cleft is not None:
+            syn_cls._initialize_cleft(syn_data, **cleft)
+        if number: syn_cls.grow(number)
+        return syn_cls
 
-        self.Synapse = self._make_synapse_class()
+    @classmethod
+    def _initialize_attachment_point(syn_cls, syn_data, index, *, constraints={}, mechanisms={}):
+        name = 'x' + str(index)
+        syn_data.add_attribute(name, dtype='Segment')
+        syn_cls._attachment_points.append(mechanisms)
+        for mechanism_name in mechanisms:
+            mechanism_name = str(mechanism_name)
+            syn_data.add_attribute(name + '_' + mechanism_name, dtype=mechanism_name)
 
-        self._initial_growth(number, number_per_neuron)
+    @classmethod
+    def _initialize_cleft(syn_cls, syn_data, *, volume, spillover_area=0.0):
+        syn_cls._cleft_volume           = float(volume)
+        syn_cls._cleft_spillover_area   = float(spillover_area)
+        syn_data.add_attribute('cleft', dtype='Extracellular')
 
-    def _make_synapse_class(self):
-        bases = [Synapse, STP]
-        synapse_data = self.database.add_class(self.name, bases)
-        synapse_data.add_attribute('presynapse',  'Segment')
-        synapse_data.add_attribute('postsynapse', 'Segment')
-        STP.initialize(synapse_data, **self.stp)
-        return synapse_data.get_instance_type()
+    def __init__(self, *attachment_points):
+        syn_cls  = type(self)
+        syn_data = syn_cls.get_database_class()
 
-    def _initial_growth(self, number, number_per_neuron):
-        # Clean the inputs.
-        assert (number is None) or (number_per_neuron is None)
-        if number_per_neuron is not None:
-            postsyn_segments    = self.constraints.get_postsynapse_candidates()
-            segment_dot_neuron  = self.database.get_data('Segment.neuron')
-            postsyn_neurons     = segment_dot_neuron[postsyn_segments]
-            num_postsyn_neurons = len(set(postsyn_neurons))
-            number              = round(number_per_neuron * num_postsyn_neurons)
-        # 
+        cleft = None
+        cleft_volume  = getattr(syn_cls, '_cleft_volume', None)
+        if cleft_volume is not None:
+            spillover_area = syn_cls._cleft_spillover_area
+            database = syn_data.get_database()
+            ECS = database.get_instance_type('Extracellular')
+            coordinates = np.zeros(3)
+            for s in attachment_points:
+                coordinates += s.coordinates
+            coordinates /= len(attachment_points)
+            self.cleft = cleft = ECS(coordinates, cleft_volume)
+            assert spillover_area == 0, 'Unimplemented!'
+
+        for index, segment in enumerate(attachment_points):
+            name = 'x' + str(index)
+            setattr(self, name, segment)
+            mechanisms = syn_cls._attachment_points[index]
+            mechanisms = segment.insert(mechanisms, outside=cleft)
+            for mechanism_name, mechanism_instance in mechanisms.items():
+                setattr(self, name + '_' + mechanism_name, mechanism_instance)
+
+    @classmethod
+    def grow(cls, number=1) -> ['Synapse']:
+        number = int(number)
+        candidates = []
+        # for at in :
+        #     cls.model.filter_segments_by_type(neuron_types, segment_types)
+        #     candidates.append()
+
+
         pairs = self.constraints.find_all_candidates()
         random.shuffle(pairs)
         # Iterate over and filter out candidates which are already taken.
@@ -70,6 +89,7 @@ class SynapseGrowthProgram:
             postsyn = index_to_object(postsyn)
             self.Synapse(presyn, postsyn)
 
+
 class SynapsesFactory(dict):
     def __init__(self, rxd_model, parameters: dict):
         super().__init__()
@@ -83,5 +103,6 @@ class SynapsesFactory(dict):
     def add_synapse_type(self, synapse_type: str, synapse_parameters: dict):
         synapse_type = str(synapse_type)
         assert synapse_type not in self
-        x = SynapseGrowthProgram(rxd_model, synapse_type, **synapse_parameters)
-        self[synapse_type] = x.Synapse
+        self[synapse_type] = Synapse._initialize(self.rxd_model, synapse_type, **synapse_parameters)
+
+
