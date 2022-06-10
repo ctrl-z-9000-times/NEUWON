6a9736ff1aea67a61438ddd08759accb2184018b
ctrl-z-9000-times <dam1784@rit.edu>
2022-06-10 19:24:48 -0400

    Fix:  nmodl_library/artificial/presynapse

--- nmodl_library/artificial/presynapse.py
+++ nmodl_library/artificial/presynapse.py
@@ -1,11 +1,9 @@
 import numpy as np
-from .stp import STP_Mixin
-from neuwon.rxd import LocalMechanismSpecification, LocalMechanismInstance
-from neuwon.database import Compute
+from neuwon.rxd import Mechanism
+from neuwon.database import Compute, Real
 
-class Presynapse(LocalMechanismSpecification):
-    def __init__(self, **parameters):
-        self._parameters = parameters
+class Presynapse(Mechanism):
+    __slots__ = ()
 
     @staticmethod
     def get_parameters():
@@ -16,47 +14,43 @@ class Presynapse(LocalMechanismSpecification):
                 'resource_recovery': 0,}
 
     @classmethod
-    def initialize(cls, model, mechanism_name):
-        p  = self._parameters
+    def initialize(cls, model, mechanism_name, **kwargs):
         db = model.get_database()
         db_class = db.add_class(mechanism_name, cls, sort_key='segment')
 
-        db_class.add_class_attribute('initial_strength', p['initial_strength'])
+        db_class.add_class_attribute('initial_strength', kwargs['initial_strength'])
         db_class.add_attribute('magnitude')
 
         db_class.add_attribute('segment', dtype='Segment')
         db_class.add_attribute('outside', dtype='Outside')
         db_class.add_attribute('_AP', dtype=np.bool)
 
-        db_class.add_class_attribute('minimum_utilization', p['minimum_utilization'],
+        db_class.add_class_attribute('minimum_utilization', kwargs['minimum_utilization'],
                 valid_range=(0, 1))
-        db_class.add_class_attribute('utilization_decay', p['utilization_decay'],
+        db_class.add_class_attribute('utilization_decay', kwargs['utilization_decay'],
                 units='ms',
                 doc='Time period of exponential decay',
                 valid_range=(0, None))
-        db_class.add_class_attribute('resource_recovery', p['resource_recovery'],
+        db_class.add_class_attribute('resource_recovery', kwargs['resource_recovery'],
                 units='ms',
                 doc='Time period of exponential recovery',
                 valid_range=(0, None))
 
-        db_class.add_attribute('utilization', p['minimum_utilization'])
+        db_class.add_attribute('utilization', kwargs['minimum_utilization'])
         db_class.add_attribute('resources', 1)
         db_class.add_attribute('last_update', 0)
 
         cls = db_class.get_instance_type()
-        cls.neurotransmitter = db.get_data('Outside.' + p['neurotransmitter'])
+        cls.neurotransmitter = db.get_data('Outside.' + kwargs['neurotransmitter'])
         return cls
 
-
-class Presynapse(LocalMechanismInstance, STP_Mixin):
-    __slots__ = ()
     def __init__(self, segment, magnitude, outside):
         self.segment = segment
         self.magnitude = magnitude * self.initial_strength
         self.outside = outside
 
     @classmethod
-    def set_time(cls):
+    def reset(cls, timestamp):
         1/0
 
     @classmethod
@@ -71,7 +65,7 @@ class Presynapse(LocalMechanismInstance, STP_Mixin):
     @Compute
     def _AP_detected(self) -> np.bool:
         AP   = (self.segment.voltage > 20)
-        edge = AP & not self._AP
+        edge = AP and not self._AP
         self._AP = AP
         return edge
 
