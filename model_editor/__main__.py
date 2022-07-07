7b49cb5f55ed2c62a5555612d7f4048d445959e5
ctrl-z-9000-times <dam1784@rit.edu>
2022-07-07 10:29:28 -0400

    modified: model_editor/__main__.py

--- model_editor/__main__.py
+++ model_editor/__main__.py
@@ -1,7 +1,29 @@
 import webbrowser
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from os.path import dirname, join
 
-website_path = join(dirname(__file__), 'index.html')
-webbrowser.open(website_path)
+root = dirname(__file__)
+
+class gui_server(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            send_data = open(join(root, self.path[1:])).read()
+            self.send_response(200)
+        except:
+            send_data = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(send_data, 'utf-8'))
+
+    def do_POST(self):
+        print(dir(self))
+
+webbrowser.open("localhost:8080/index.html")
+
+httpd = HTTPServer(('localhost', 8080), gui_server)
+httpd.serve_forever()
 
 # TODO: Allow CLI arg containing saved file and start with that file opened?
