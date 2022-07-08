b19e5b2895e440e4a47f5a3ead9a93cf7a13fa29
ctrl-z-9000-times <dam1784@rit.edu>
2022-07-08 09:07:33 -0400

    " -> '

--- neuwon/rxd/nmodl/code_gen.py
+++ neuwon/rxd/nmodl/code_gen.py
@@ -6,21 +6,21 @@ import textwrap
 import sympy.core.basic
 import sympy.printing.pycode as sympy_to_pycode
 
-def to_python(self, indent=""):
+def to_python(self, indent=''):
     """ Argument self is any parser CodeBlock or Statement. """
-    py = ""
+    py = ''
     if isinstance(self, CodeBlock):
         for stmt in self.statements:
             py += to_python(stmt, indent)
         if not self.statements:
-            py += indent + "pass"
+            py += indent + 'pass'
     elif isinstance(self, IfStatement):
-        py += indent + "if %s:\n"%sympy_to_pycode(self.condition)
-        py += to_python(self.main_block, indent + "    ")
-        assert not self.elif_blocks, "Unimplemented."
+        py += indent + 'if %s:\n'%sympy_to_pycode(self.condition)
+        py += to_python(self.main_block, indent + '    ')
+        assert not self.elif_blocks, 'Unimplemented.'
         if self.else_block is not None:
-            py += indent + "else:\n"
-            py += to_python(self.else_block, indent + "    ")
+            py += indent + 'else:\n'
+            py += to_python(self.else_block, indent + '    ')
     elif isinstance(self, AssignStatement):
         try:
             if isinstance(self.rhs, sympy.core.basic.Basic):
@@ -30,34 +30,34 @@ def to_python(self, indent=""):
                 rhs = str(self.rhs)
             py += indent + self.lhsn + self.operation + rhs
         except Exception:
-            print("Failed at:", repr(self), flush=True)
+            print('Failed at:', repr(self), flush=True)
             raise
     elif isinstance(self, ConserveStatement):
-        py += indent + "pass    # Warning: ignored CONSERVE statement here."
+        py += indent + 'pass    # Warning: ignored CONSERVE statement here.'
     elif isinstance(self, SolveStatement):
         solve_block = self.block
         arguments = ', '.join(sorted(solve_block.arguments))
-        py += f"{indent}{solve_block.name}({arguments})"
+        py += f'{indent}{solve_block.name}({arguments})'
     elif isinstance(self, str):
         py += textwrap.indent(self, indent)
     else:
         raise NotImplementedError(type(self))
-    return py.rstrip() + "\n"
+    return py.rstrip() + '\n'
 
 def exec_string(python, globals_, locals_=None):
     if not isinstance(python, str): python = str(python)
     with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as f:
         f.write(python)
         f.flush()
-    globals_.setdefault("math", math)
+    globals_.setdefault('math', math)
     try:
         bytecode = compile(python, f.name, mode='exec')
         exec(bytecode, globals_, locals_)
     except:
-        for noshow in ("__builtins__", "math"):
+        for noshow in ('__builtins__', 'math'):
             globals_[noshow] = '...'
         err_msg = "Error while exec'ing the following python program:\n" + python
-        err_msg + "\nglobals(): %s"%repr(globals_)
-        err_msg + "\nlocals(): %s"%repr(locals_)
+        err_msg + '\nglobals(): %s'%repr(globals_)
+        err_msg + '\nlocals(): %s'%repr(locals_)
         print(err_msg, flush=True)
         raise
